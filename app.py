83704717e1eed64161d1ba1446880b349447b253
primboi <prianshmittra@gmail.com>
2022-05-18 19:51:14 +0000

    OK

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 # imports
-from flask import Flask, url_for, render_template, redirect, request, make_response # more imports can be added 
+# more imports can be added
+from flask import Flask, url_for, render_template, redirect, request, make_response
 import pymongo
 import random
 import requests
 import time
-# from bitcoin import * 
+# from bitcoin import *
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import json
 # initializers
 app = Flask(__name__)
-rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8000"%("username", "password")) 
+rpc_connection = AuthServiceProxy(
+    "http://%s:%s@127.0.0.1:8000" % ("username", "password"))
+
+
 def get_database():
     from pymongo import MongoClient
 
@@ -18,10 +22,16 @@ def get_database():
     client = MongoClient(CONNECTION_STRING)
 
     return client['aviandex']
+
+
 dbname = get_database()
 tradedb = dbname["trades"]
+assetdb = dbname["assets"]
+
 def price(pair):
     pass
+
+
 def uid():
     # make a array with all the lowercase letters of the alphabet
     uid_string = ""
@@ -39,20 +49,25 @@ def uid():
             pass
     return uid_string
 # dex start
+
+
 @app.route('/')
 def index():
     while True:
         try:
-            blockcount = rpc_connection.batch_([["blockcount"]])
+            blockcount = rpc_connection.batch_([["getblockcount"]])
             break
         except:
             pass
-    while True:
-        try:
-            assets = rpc_connection.batch_([["listmyassets"]])[0]
-            break
-        except:
-            pass
+            #assets = rpc_connection.batch_([["listmyassets"]])[0]
+            # assets = {
+            #     "DEX": 5000000.0000,
+            #     "DEX!": 1
+            # }
+    asset = assetdb.find()
+    assets = []
+    for name in asset:
+        assets.append(name["asset_name"])
     #assets = json.load(open('trade.json', "r"))
     #result = assets["assets"]
     get_op = []
@@ -68,8 +83,11 @@ def index():
     #         pass
     #     else:
     #         get_op.append(result[0][i])
-    #     i+=1
-    return render_template("index.html", assets=assets, lenassets=len(result), lenwall=len_bg, blockcount=blockcount)
+    #     i+=
+    # key_list = [key for key in assets]
+    # return "Printed"
+    #return {"data":assets}
+    return render_template("index.html", lenassets=len(assets), lenwall=len_bg, blockcount=blockcount, assets=assets)
 # Command Section (TESTING, DELETE ONCE SEND WORKS!) - Deleted
 # @app.route('/command/<command>')
 # def cmd(command):
@@ -78,7 +96,9 @@ def index():
 #     except:
 #         return {"error":"command not found"}
 #     return {"success": True, "data":result}
-#@app.route('/send', methods=["GET", "POST"])
+# @app.route('/send', methods=["GET", "POST"])
+
+
 def send():
     if request.method == "POST":
         addr = request.form["addr"]
@@ -88,8 +108,10 @@ def send():
             try:
                 result = rpc_connection.batch_([["sendtoaddress", addr, 5]])
             except Exception as e:
-                return redirect(url_for("index", message="Unable to send because it was an invalid address" , type="error"))
+                return redirect(url_for("index", message="Unable to send because it was an invalid address", type="error"))
             return redirect(url_for("index", message="Sent 5 Testnet Avian to <b>" + addr + "</b>", type="success"))
+
+
 @app.route('/connect', methods=["POST"])
 def connectwallet():
     if request.method == "POST":
@@ -100,6 +122,8 @@ def connectwallet():
             resp = make_response(redirect(url_for("index")))
             resp.set_cookie('wallet', wallet_addr)
             return resp
+
+
 @app.route('/trade', methods=["POST"])
 def trade():
     tradep1 = request.form["asset"]
@@ -119,11 +143,14 @@ def trade():
             break
         except:
             pass
-    tradedb.insert_one({"uid":uids, "pair":pair, "type":"trade", "amountp1":tradep1_amt, "amountp2":tradep1_amt*price, "txid":"-", "txidaddress":new_addr, "address":request.cookies.get('wallet'), "status":"pending", "time":time.time()})
+    tradedb.insert_one({"uid": uids, "pair": pair, "type": "trade", "amountp1": tradep1_amt, "amountp2": tradep1_amt*price,
+                       "txid": "-", "txidaddress": new_addr, "address": request.cookies.get('wallet'), "status": "pending", "time": time.time()})
     return redirect(url_for("payment", uid=uids))
+
+
 @app.route('/trade/<uid>', methods=["GET", "POST"])
 def payment(uid):
-    finds = tradedb.find({"uid":uid})
+    finds = tradedb.find({"uid": uid})
     finders = []
     for find in finds:
         finders.append(find)
@@ -131,51 +158,64 @@ def payment(uid):
         return redirect(url_for("index", message="Unable to find trade", type="error"))
     else:
         return render_template("trade.html", msgtype="incompletetx", uid=uid, pair=(finders[0]["pair"]).split("-"), amount=finders[0]["amount"], find=finders[0])
+
+
 @app.route('/price/<pair>')
 def price(pair):
     assets = json.load(open('trade.json', "r"))
     result = assets["assets"]
     all_assets = ''
     i = 0
-    for i in range(len(result)): 
+    for i in range(len(result)):
         if pair == result[i][0]:
             return result[i][2]
-        i+=1
+        i += 1
     return "Not Found"
+
+
 @app.route('/get/status/<uid>', methods=["GET"])
 def gettx(uid):
     # file = open("test.txt", "r")
     # contents = file.read()
-    
-    get_uid = tradedb.find({"uid":uid})
+
+    get_uid = tradedb.find({"uid": uid})
     get_ui = []
     for uids in get_uid:
         get_ui.append(uids)
     if get_ui == []:
-        return {"error":"Unable to find trade"}
+        return {"error": "Unable to find trade"}
     else:
-        result = requests.get("https://testnet.avn.network/ext/getaddress/" + get_ui[0]["txidaddress"][0]).json()
+        result = requests.get(
+            "https://testnet.avn.network/ext/getaddress/" + get_ui[0]["txidaddress"][0]).json()
         try:
             txid = result["error"]
-            return {"status":"pending"}
+            return {"status": "pending"}
         except:
-                txid_arr = result["last_txs"]
-                txid = []
-                for txids in txid_arr:
-                    if txids["type"] == "vout":
-                        txid.append(txids["addresses"])
-                    else:
-                        pass
-                if txid == []:
-                    return {"status":"pending"}
+            txid_arr = result["last_txs"]
+            txid = []
+            for txids in txid_arr:
+                if txids["type"] == "vout":
+                    txid.append(txids["addresses"])
                 else:
-                    if get_ui[0]["status"] == "pending":
-                                send_coin = rpc_connection.batch_([["sendfrom", "", get_ui[0]["address"], get_ui[0]["amount"]]])
-                                print(send_coin)
-                                tradedb.update_one({"uid":uid}, {"$set":{"txid":txid[0], "status":"complete"}})
-                        #return {"status":"complete", "txid":txid[0], "balance":result["received"]}
-            # except: 
+                    pass
+            if txid == []:
+                return {"status": "pending"}
+            else:
+                if get_ui[0]["status"] == "pending":
+                    while True:
+                        try:
+                            send_coin = rpc_connection.batch_(
+                                [["sendfrom", "", get_ui[0]["address"], get_ui[0]["amount"]]])
+                            print(send_coin)
+                            break
+                        except:
+                            pass
+                    tradedb.update_one(
+                        {"uid": uid}, {"$set": {"txid": txid[0], "status": "complete"}})
+                return {"status": "complete", "txid": txid[0]}
+
+                # return {"status":"complete", "txid":txid[0], "balance":result["received"]}
+            # except:
             #     return {"error":"Unable to find TX"}
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
-
